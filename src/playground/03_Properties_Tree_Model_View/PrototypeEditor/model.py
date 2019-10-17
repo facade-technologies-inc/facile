96fd3b52888587bdb4e580646b0bfa7e2a5661b2
smalbadger <smalbadger@email.arizona.edu>
2019-10-17 12:19:28 -0700

    Fixed bugs in the model. Now the TreeView displays it properly.

--- src/playground/03_Properties_Tree_Model_View/PrototypeEditor/model.py
+++ src/playground/03_Properties_Tree_Model_View/PrototypeEditor/model.py
@@ -1,4 +1,5 @@
 from PySide2.QtCore import QAbstractItemModel, QModelIndex, Qt
+from PySide2.QtGui import QColor
 import data
 
 class PropModel(QAbstractItemModel):
@@ -17,7 +18,7 @@ class PropModel(QAbstractItemModel):
         else:
             parentData = parent.internalPointer()
             if parentData in self._propData.getCategories():
-                internalData = self._propData.getCategoryProperties(parentData)
+                internalData = self._propData.getCategoryProperties(parentData)[row]
 
             else:
                 return QModelIndex()
@@ -40,20 +41,11 @@ class PropModel(QAbstractItemModel):
 
     def columnCount(self, parent):
         """Purpose of this function is to return the number of columns for the children of a given parent"""
-        if not parent.isValid():
-            return 2 #1
-
-        parentData = parent.internalPointer()
-
-        if parentData in self._propData.getCategories():
-            return 2
-        else:
-            return 2 #0
+        return 2
 
     def rowCount(self, parent):
         """Purpose of this function is to return the number of children of a given parent"""
         if not parent.isValid():
-            return 3
             numCategories = self._propData.getNumCategories()
             return numCategories
 
@@ -70,28 +62,63 @@ class PropModel(QAbstractItemModel):
         if not index.isValid():
             return QModelIndex()
 
-        if not role != Qt.DisplayRole:
-            return None
-
         row = index.row()
         col = index.column()
         data = index.internalPointer()
 
-        if data in self._propData.getCategories():
-            return data
-        else:
-            col = index.column()
-            if col == 0:
-                return data.getName()
-            elif col == 1:
-                return data.getValue()
+        if role == Qt.DisplayRole:
+            if data in self._propData.getCategories():
+                if col == 0:
+                    return data
+                else:
+                    return None
             else:
-                return None
+                col = index.column()
+                if col == 0:
+                    return data.getName()
+                elif col == 1:
+                    return str(data.getValue())
+                else:
+                    return None
+                
+        elif role == Qt.BackgroundRole:
+            if data in self._propData.getCategories():
+                return QColor(Qt.yellow)
+            else:
+                shade = row%2 * 25
+                return QColor(100+shade, 150+shade, 200+shade)
 
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return ["Name","Value"][section]
         return None
+    
+    
+    
+    #########################################################################
+    def traverse(self):
+        """ This method is used for debugging by mimicing how a view might query the model for data."""
+        parent = QModelIndex()
+        work = [parent]
+        
+        while len(work) > 0:
+            cur = work.pop()
+            
+            curRow = cur.row()
+            curCol = cur.column()
+            curData = self.data(cur, Qt.DisplayRole)
+            if cur.isValid():
+                print(curRow, curCol, curData)
+                pass
+                
+            rows = self.rowCount(cur)
+            cols = self.columnCount(cur)
+            for r in range(rows):
+                for c in range(cols):
+                    work.append(self.index(r, c, cur))
+        
+    
+        
 
 
 
