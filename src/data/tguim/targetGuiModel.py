00fa1e78c1f361c697957cc3ce347c07a5a65798
sfarrisUA <sfarris@email.arizona.edu>
2019-11-09 11:54:24 -0700

    Added TargetGuiModel class.

---
+++ src/data/tguim/targetGuiModel.py
@@ -0,0 +1,61 @@
+
+# TODO: Import Ramos's QGraphicsScene class. ???
+# TODO: Import corresponding QModel classes for the componentTree and the visibility behaviors.
+# TODO: Import GuiComponent class.
+# TODO: Import SuperToken class
+
+
+class TargetGuiModel:
+    def __init__(self):
+        self._root = None  # TODO: Initialize the root.
+        self._componentTreeQModel = None  # TODO: Initialize new QModel here.
+        self._visibilityBehaviorsQModel = None   # TODO: Initialize new QModel here.
+        self._scene = None  # TODO: Use Ramos' class here. Maybe pass as param?
+        self._components = {}  # TODO: Add root "component" to the dict? Maybe it needs to remain inaccessible.
+        self._visibilityBehaviors = {}
+
+    # TODO: This getter may not be necessary.
+    # def getRoot(self):
+    #     # if self._root.isDeleted():
+    #     #     return None
+    #     return self._root
+
+    # TODO: Want the root to always remain constant.
+    # def setRoot(self, newRoot):
+    #     if self._root is None:
+    #         self._root = newRoot
+    #     elif self._root.isDeleted():
+    #         self._root = newRoot
+    #     self._scene.addItem(newRoot.getNodeItem())
+
+    def getComponentTreeQModel(self):
+        return self._componentTreeQModel
+
+    def getScene(self):
+        return self._scene
+
+    def getComponents(self):
+        return self._components
+
+    def getComponent(self, iD):
+        if iD in self._components:
+            return self._components[iD]
+        else:
+            return None
+
+    # Slot function for when the Observer emits the "newSuperToken" signal.
+    def createComponent(self, superToken):
+        pass  # TODO: define this function.
+
+    def getVisibilityBehaviors(self):
+        return self._visibilityBehaviors
+
+    def getVisibilityBehavior(self, iD):
+        if iD in self._visibilityBehaviors:
+            return self._visibilityBehaviors[iD]
+        else:
+            return None
+
+    def addVisibilityBehavior(self, newVisBehavior):
+        if newVisBehavior.getId() not in self._visibilityBehaviors:
+            self._visibilityBehaviors[newVisBehavior.getId()] = newVisBehavior
