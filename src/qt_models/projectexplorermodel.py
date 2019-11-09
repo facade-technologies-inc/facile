c392cdae09aca262e910f348f9c96ef6bfbd073d
smalbadger <smalbadger@email.arizona.edu>
2019-11-09 12:11:02 -0700

    Project Explorer Qt Model is filled out - waiting on Sean to get Target GUI Model classes done.

--- src/qt_models/projectexplorermodel.py
+++ src/qt_models/projectexplorermodel.py
@@ -1,7 +1,13 @@
 from PySide2.QtGui import QStandardItem
 from PySide2.QtCore import QAbstractItemModel, QModelIndex, QPersistentModelIndex, Qt, Signal
+from data.project import Project
+from data.tguim.guicomponent import GUIComponent
+from data.tguim.visibilitybehavior import VisibilityBehavior
+from data.apim.actionpipeline import ActionPipeline
 
 # TODO: Remove this once we have the actual models from the target GUI model and the the api model
+
+
 def pop_standard_model(model):
 	for i in range(3):
 		parent1 = QStandardItem('Family {}. Some long status text for sp'.format(i))
@@ -12,36 +18,11 @@ def pop_standard_model(model):
 			parent1.appendRow([child1, child2])
 		model.appendRow([parent1, parent2])
 
-def debug(func):
-	def wrapper(*args, **kwargs):
-		ret = func(*args, **kwargs)
-		internalPointer = ret.internalPointer()
-		if isinstance(internalPointer, str):
-			print("{} {} {}".format(ret.row(), ret.column(), internalPointer))
-		else:
-			print("{} {} {}".format(ret.row(), ret.column(), ret.internalPointer()[1]))
-		return ret
-	
-	return wrapper
-	
+# TODO: Update this file once Sean integrates his code.
+
 
 class ProjectExplorerModel(QAbstractItemModel):
-	"""
-	This class is used to access multiple models as one. It will primarily be used as the model in the Project Explorer.
 	
-	How it works:
-		References to multiple other models are stored as attributes of this class.
-		
-		Each index in this model will either be carrying a string (which acts as a label), or a reference to an index
-		from one of the inner models.
-		
-		The indexes from the inner models are converted to QPersistentModelIndexes so that we can store them in indexes
-		of this model.
-		
-		We have to store references to the QPersistentModelIndexes so Python's the garbage collector doesn't delete them
-		too early. This requires managing references to indexes as they are created because we don't want to store
-		infinite indexes.
-	"""
 	componentSelected = Signal(object)
 	behaviorSelected = Signal(object)
 	pipelineSelected = Signal(object)
@@ -50,6 +31,10 @@ class ProjectExplorerModel(QAbstractItemModel):
 	COMPONENT_LABEL = "GUI Components"
 	BEHAVIOR_LABEL = "Visibility Behaviors"
 	PIPELINE_LABEL = "Action Pipelines"
+	
+	NO_COMPONENTS_LABEL = "No GUI Components Yet."
+	NO_BEHAVIORS_LABEL = "No Visibility Behaviors Yet."
+	NO_PIPELINES_LABEL = "No Action Pipelines Yet."
 	
 	TARGET_GUI_ROW = 0
 	COMPONENT_ROW = 0
@@ -66,10 +51,6 @@ class ProjectExplorerModel(QAbstractItemModel):
 		def __init__(self, msg):
 			Exception.__init__(self, msg)
 			
-	class UnknownModelException(Exception):
-		def __init__(self, msg):
-			Exception.__init__(self, msg)
-			
 	class UnsupportedTypeException(Exception):
 		def __init__(self, msg):
 			Exception.__init__(self, msg)
@@ -81,13 +62,10 @@ class ProjectExplorerModel(QAbstractItemModel):
 	#           END EXCEPTION DEFINITIONS           #
 	#################################################
 
-	def __init__(self, componentsModel, behaviorsModel, apiModel):
+	def __init__(self, project: Project) -> 'ProjectExplorerModel':
 		QAbstractItemModel.__init__(self)
-		self._componentsModel = componentsModel
-		self._behaviorsModel = behaviorsModel
-		self._apiModel = apiModel
+		self._project = project
 	
-	@debug
 	def index(self, row: int, column: int, parent: QModelIndex) -> QModelIndex:
 		if not self.hasIndex(row, column, parent):
 			return QModelIndex()
@@ -101,12 +79,12 @@ class ProjectExplorerModel(QAbstractItemModel):
 			else:
 				return QModelIndex()
 
-		data = parent.internalPointer()
-		
+		parentData = parent.internalPointer()
+	
 		# If the parent index holds a string, we'll be returning an index that holds either another string
 		# or a top-level index of a sub-model.
-		if isinstance(data, str):
-			if data == ProjectExplorerModel.TARGET_GUI_LABEL:
+		if isinstance(parentData, str):
+			if parentData == ProjectExplorerModel.TARGET_GUI_LABEL:
 				if row == ProjectExplorerModel.COMPONENT_ROW:
 					return self.createIndex(row, column, ProjectExplorerModel.COMPONENT_LABEL)
 				elif row == ProjectExplorerModel.BEHAVIOR_ROW:
@@ -114,28 +92,44 @@ class ProjectExplorerModel(QAbstractItemModel):
 				else:
 					return QModelIndex()
 	
-			elif data == ProjectExplorerModel.COMPONENT_LABEL:
-				model = self._componentsModel
-			elif data == ProjectExplorerModel.BEHAVIOR_LABEL:
-				model = self._behaviorsModel
-			elif data == ProjectExplorerModel.PIPELINE_LABEL:
-				model = self._apiModel
+			elif parentData == ProjectExplorerModel.COMPONENT_LABEL:
+				
+				if self._project.getTargetGUIModel().getRoot().getNumChildren() == 0:
+					return self.createIndex(row, column, (ProjectExplorerModel.NO_COMPONENTS_LABEL, ProjectExplorerModel.COMPONENT_LABEL, 0))
+				return self.createIndex(row, column, self._project.getTargetGUIModel().getRoot().getNthChild(row))
+			elif parentData == ProjectExplorerModel.BEHAVIOR_LABEL:
+				
+				if self._project.getTargetGUIModel().getNumVisibilityBehaviors() == 0:
+					return self.createIndex(row, column, (ProjectExplorerModel.NO_BEHAVIORS_LABEL, ProjectExplorerModel.BEHAVIOR_LABEL, q))
+				return self.createIndex(row, column, self._project.getTargetGUIModel().getNthBehavior(row))
+			elif parentData == ProjectExplorerModel.PIPELINE_LABEL:
+				# TODO: replace this once action pipelines are implemented
+				return self.createIndex(row, column, (ProjectExplorerModel.NO_PIPELINES_LABEL, parentData, 1))
 			else:
-				raise ProjectExplorerModel.InvalidLabelException("Unsupported data string: {}".format(data))
+				raise ProjectExplorerModel.InvalidLabelException("Unsupported data string: {}".format(parentData))
 			
-			return self.createIndex(row, column, (model, [row]))
+		elif isinstance(parentData, GUIComponent):
 			
-		elif isinstance(data, tuple):
-			model = data[ProjectExplorerModel.MODEL]
-			path = data[ProjectExplorerModel.PATH] + [row]
+			return self.createIndex(row, column, parentData.getNthChild(row))
 			
-			if model not in (self._componentsModel, self._behaviorsModel, self._apiModel):
-				raise ProjectExplorerModel.UnknownModelException("Unsupported model: {}".format(data.model()))
+		elif isinstance(parentData, VisibilityBehavior):
+			if row == 0:
+				
+				return self.createIndex(row, column, (parentData.getFromComponent(), parentData))
+			if row == 1:
+				
+				return self.createIndex(row, column, (parentData.getToComponent(), parentData))
 			
-			return self.createIndex(row, column, (model, path))
+		elif isinstance(parentData, ActionPipeline):
+			# TODO: replace this once action pipelines are implemented
+			pass
+			
+		elif isinstance(parentData, tuple):
+			# If the internal pointer is a tuple, it should have no children.
+			pass
 		
 		else:
-			raise ProjectExplorerModel.UnsupportedTypeException("Unsupported data type in index: {}".format(type(data)))
+			raise ProjectExplorerModel.UnsupportedTypeException("Unsupported data type in index: {}".format(type(parentData)))
 		
 	def parent(self, index: QModelIndex) -> QModelIndex:
 		if not index.isValid():
@@ -149,29 +143,34 @@ class ProjectExplorerModel(QAbstractItemModel):
 			elif data in (ProjectExplorerModel.COMPONENT_LABEL, ProjectExplorerModel.BEHAVIOR_LABEL):
 				return self.createIndex(0, 0, ProjectExplorerModel.TARGET_GUI_LABEL)
 			
+		elif isinstance(data, GUIComponent):
+			
+			parentComponent = data.getParent()
+			
+			if parentComponent is self._project.getTargetGUIModel().getRoot():
+				return self.createIndex(0, 0, ProjectExplorerModel.COMPONENT_LABEL)
+			else:
+				
+				return self.createIndex(parentComponent.getRow(), 0, parentComponent)
+			
+		elif isinstance(data, VisibilityBehavior):
+			
+			return self.createIndex(1, 0, ProjectExplorerModel.BEHAVIOR_LABEL)
+		
+		elif isinstance(data, ActionPipeline):
+			# TODO: replace this once action pipelines are implemented
+			pass
+		
 		elif isinstance(data, tuple):
-			model = data[ProjectExplorerModel.MODEL]
-			path = data[ProjectExplorerModel.PATH][:-1]
+			innerData = data[0]
+			if isinstance(innerData, str):
+				return self.createIndex(data[2], 0, data[1])
 			
-			if model not in (self._componentsModel, self._behaviorsModel, self._apiModel):
-				raise ProjectExplorerModel.UnknownModelException("Unsupported model: {}".format(data.model()))
+			elif isinstance(innerData, GUIComponent):
+				# data[1] will be a visibility behavior
+				
+				return self.createIndex(data[1].getRow(), 0, data[1])
 			
-			parent = ProjectExplorerModel.getInternalModelIndex(model, path)
-			
-			if not parent.isValid():
-				if data.model() is self._componentsModel:
-					label = ProjectExplorerModel.COMPONENT_LABEL
-					row = ProjectExplorerModel.COMPONENT_ROW
-				elif data.model() is self._behaviorsModel:
-					label = ProjectExplorerModel.BEHAVIOR_LABEL
-					row = ProjectExplorerModel.BEHAVIOR_ROW
-				elif data.model() is self._apiModel:
-					label = ProjectExplorerModel.PIPELINE_LABEL
-					row = ProjectExplorerModel.PIPELINE_ROW
-				return self.createIndex(row, 0, label)
-			
-			return self.createIndex(path[-1], 0, (model, path))
-
 		else:
 			raise ProjectExplorerModel.UnsupportedTypeException("Unsupported data type in index: {}".format(type(data)))
 
@@ -185,21 +184,28 @@ class ProjectExplorerModel(QAbstractItemModel):
 				return 2
 	
 			elif data == ProjectExplorerModel.COMPONENT_LABEL:
-				return self._componentsModel.rowCount(QModelIndex())
+				return max(1, self._project.getTargetGUIModel().getRoot().getNumChildren())
 	
 			elif data == ProjectExplorerModel.BEHAVIOR_LABEL:
-				return self._behaviorsModel.rowCount(QModelIndex())
+				return max(1, self._project.getTargetGUIModel().getNumVisibilityBehaviors())
 	
 			elif data == ProjectExplorerModel.PIPELINE_LABEL:
-				return self._apiModel.rowCount(QModelIndex())
+				# TODO: replace this once action pipelines are implemented
+				return 1
 			
+		elif isinstance(data, GUIComponent):
+			return data.getNumChildren()
+		
+		elif isinstance(data, VisibilityBehavior):
+			return 2
+		
 		elif isinstance(data, tuple):
-			model = data[ProjectExplorerModel.MODEL]
-			path = data[ProjectExplorerModel.PATH]
-			print(type(model), type(path))
-			internalIndex = ProjectExplorerModel.getInternalModelIndex(model, path)
-			return model.rowCount(internalIndex)
+			return 0
 			
+		elif isinstance(data, ActionPipeline):
+			# TODO: replace this once action pipelines are implemented
+			pass
+		
 		else:
 			raise ProjectExplorerModel.UnsupportedTypeException("Unsupported data type in index: {}".format(type(data)))
 
@@ -229,12 +235,55 @@ class ProjectExplorerModel(QAbstractItemModel):
 				return data
 			else:
 				return None
-				
+		
+		elif isinstance(data, GUIComponent):
+			if col == 0:
+				return data.getName()
+			elif col == 1:
+				return data.getType()
+			else:
+				return None
+		
+		elif isinstance(data, VisibilityBehavior):
+			if col == 0:
+				return data.getName()
+			elif col == 1:
+				return data.getType()
+			else:
+				return None
+		
+		elif isinstance(data, ActionPipeline):
+			if col == 0:
+				return data.getName()
+			elif col == 1:
+				return data.getType()
+			else:
+				return None
+			
 		elif isinstance(data, tuple):
-			model = data[ProjectExplorerModel.MODEL]
-			path = data[ProjectExplorerModel.PATH]
-			internalIndex = ProjectExplorerModel.getInternalModelIndex(model, path, column = index.column())
-			return model.data(internalIndex, role)
+			innerData = data[0]
+			if isinstance(innerData, str):
+				if col == 0:
+					return innerData
+				else:
+					return None
+				
+			if isinstance(innerData, GUIComponent):
+				if row == 0:
+					if col == 0:
+						return "From"
+					elif col == 1:
+						return innerData.getName()
+					else:
+						return None
+				if row == 1:
+					if col == 0:
+						return "To"
+					elif col == 1:
+						return innerData.getName()
+					else:
+						return None
+		
 		else:
 			raise ProjectExplorerModel.UnsupportedTypeException("Unsupported data type in index: {}".format(type(data)))
 		
@@ -269,23 +318,3 @@ class ProjectExplorerModel(QAbstractItemModel):
 
 			else:
 				raise ProjectExplorerModel.InvalidSelectionException("Multiple row selection is not supported")
-
-	@staticmethod
-	def getInternalModelIndex(internalModel, path, column=0):
-		curIndex = QModelIndex() #Start with root
-		for i in range(path):
-			row = path[i]
-			if i == len(path) - 1:
-				col = column
-			else:
-				col = 0
-				
-			if internalModel.rowCount(curIndex) <= row:
-				raise Exception("Bad path")
-			if internalModel.columnCount(curIndex) <= col:
-				raise Exception("Bad path")
-				
-			curIndex = internalModel.index(row, col, curIndex)
-			
-		return curIndex
-			
