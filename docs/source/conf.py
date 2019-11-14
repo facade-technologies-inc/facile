23bb20e44e93e3cfbf40c50182ea3c8803bf15ed
smalbadger <smalbadger@email.arizona.edu>
2019-11-14 12:22:46 -0700

    Set up Sphinx and can generate documentation in latex or HTML format with no content. AutoAPI is set up, but needs some love.

---
+++ docs/source/conf.py
@@ -0,0 +1,106 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Facile'
+copyright = '2019, Facade Technologies Inc.'
+author = 'Facade Technologies Inc.'
+version = "0.2.0"
+release = "0.2.0"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.inheritance_diagram',
+    'autoapi.sphinx',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.graphviz'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# -- Options for AutoAPI -----------------------------------------------------
+mods = {
+    'gui': ['copyprojectdialog',
+            'newprojectdialog',
+            'manageprojectdialog',
+            'facileview'],
+    'libs': ['bitness'],
+    'qt_models': ['projectexplorermodel',
+                  'propeditormodel',
+                  'propeditordelegate'],
+    'tguiil': ['application',
+               'explorer',
+               'observer',
+               'supertokens',
+               'tokens'],
+    'graphics/tguim': ['componentgraphics',
+                       'tscene',
+                       'visibilitybehaviorgraphics'],
+    'data': ['entity',
+             'project',
+             'properties',
+             'property'],
+    'data/tguim': ['component',
+                   'condition',
+                   'targetguimodel',
+                   'visibilitybehavior']
+}
+
+autoapi_modules = {}
+for dir in mods:
+    for mod in mods[dir]:
+        modName = dir.replace("/", ".") + "." + mod
+        autoapi_modules[modName] = {'override': True, 'output': "auto/" + dir}
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Set Facade Technologies Inc. logo
+html_logo = "Facade @0.5x.png"
+html_favicon = "facade_logo_favicon.ico"
+
+# -- Options for LaTeX output -------------------------------------------------
+
+# Set Facade Technologies Inc. logo
+latex_logo = "facade_logo_small.png"
+latex_favicon = "facade_logo_favicon.ico"
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. Facile documentation master file, created by
+   sphinx-quickstart on Wed Nov 13 10:11:36 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Facile's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   auto/data/data.project
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
