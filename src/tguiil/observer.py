d57ae4b287ad12e7739f99f9ecb576603e97ef18
smalbadger <smalbadger@email.arizona.edu>
2019-11-15 00:35:50 -0700

    Completed observer. Waiting on Tokens to be finished.

--- src/tguiil/observer.py
+++ src/tguiil/observer.py
@@ -22,6 +22,9 @@ This module contains the Observer class, which watches the target GUI for change
 """
 
 import psutil
+
+import pywinauto
+
 from PySide2.QtCore import QThread, Signal
 
 from tguiil.application import Application
@@ -56,64 +59,144 @@ class Observer(QThread):
         """
         self._process = psutil.Process(processID)
         self._backend = backend
-
-        # We'll create a tree of super tokens by using a dictionary rather than keeping children and parent references
-        # inside of the super tokens. The benefit of this is that the Target GUI Model is the only true tree structure.
-        # That way, we don't have to make sure 2 trees are synced all the time.
-        root = SuperToken()
-        self._childMapping = {root: []}  # maps each super token to its list of children.
-        self._parentMapping = {root: None}   # maps each super token to its parent.
+        self._superTokens = []  # stores list of all SuperTokens
         
     def run(self) -> int:
         """
-        The starting point for the thread after calling start()
+        DO NOT CALL THIS METHOD. This method is run in a new thread when the start method is called.
         
-        :return: the exit code
+        :return: the exit code of the thread which should be 0.
         :rtype: int
         """
         app = Application(backend=self._backend)
         app.setProcess(self._process)
         while self._process.is_running():
-            work = app.windows()[:]
+            
+            # work acts as a stack. Each element is a 2-tuple where the first element
+            # is a GUI component and the second element is the parent super token.
+            work = [(win, None) for win in app.windows()]
             while len(work) > 0:
-                curComponent = work.pop()
+                curComponent, parentSuperToken = work.pop()
+                token = self.createToken(curComponent)
+                matchedSuperToken = self.matchToSuperToken(token)
+
                 children = curComponent.children()
                 for child in children:
-                    work.append(child)
-                
-                # Information we can get about any element
-                id = curComponent.control_id()  ####store this, required, int
-                isDialog = curComponent.is_dialog()  ####store this, required, bool
-                isEnabled = curComponent.is_enabled()  ####store this, required, bool
-                isVisible = curComponent.is_visible()  ####store this, required, bool
-                parent = curComponent.parent()
-                parentTitle = parent.window_text()  ####store this, optional, str
-                parentType = parent.friendly_class_name()  ####store this, optional, str
-                topLevelParent = curComponent.top_level_parent()
-                topLevelParentTitle = topLevelParent.window_text()  ####store this, optional, str
-                topLevelParentType = topLevelParent.friendly_class_name()  ####store this, optional, str
-                processID = curComponent.process_id()  ####store this, required, int
-                rectangle = curComponent.rectangle()  ####store this, required, win32structures.RECT
-                texts = curComponent.texts()[1:]  ####store this, required, list[str]
-                title = curComponent.window_text()  ####store this, required, str
-                numControls = curComponent.control_count()  ####store this, required, int
-                image = curComponent.capture_as_image()  ####store this, optional, PIL.Image
-                className = curComponent.friendly_class_name()  ####store this, required, str
-                
-                # additional information we can get about uia elements
-                autoID = curComponent.automation_id()  ####store this, optional, int
-                childrenTexts = curComponent.children_texts()  ####store this, optional, list[str]
-                expandState = curComponent.get_expand_state()  ####store this, optional, int
-                shownState = curComponent.get_show_state()  ####store this, optional, int
-                
-                
-                
-                
-                
-                #TODO:
-                # 1. create token
-                # 2. find the new token in super token tree
-                #       a. if exact match is found, move on
-                #       b. if close match is found, add the token to the close super token
-                #       c. if no match is found, create a new super token and emit the newSuperToken signal
-                
+                    work.append((child, matchedSuperToken))
+    
+    def createToken(self, component: pywinauto.base_wrapper) -> Token:
+        """
+        Create a token from a pywinauto control.
+        
+        :param component: A pywinauto control from the target GUI.
+        :type component: pywinauto.base_wrapper
+        :return: The token that was created from the pywinauto control.
+        :rtype: Token
+        """
+
+        parent = component.parent()
+        topLevelParent = component.top_level_parent()
+        
+        # Information we can get about any element
+        id = component.control_id()  ####store this, required, int
+        isDialog = component.is_dialog()  ####store this, required, bool
+        isEnabled = component.is_enabled()  ####store this, required, bool
+        isVisible = component.is_visible()  ####store this, required, bool
+        parentTitle = parent.window_text()  ####store this, optional, str
+        parentType = parent.friendly_class_name()  ####store this, optional, str
+        topLevelParentTitle = topLevelParent.window_text()  ####store this, optional, str
+        topLevelParentType = topLevelParent.friendly_class_name()  ####store this, optional, str
+        processID = component.process_id()  ####store this, required, int
+        rectangle = component.rectangle()  ####store this, required, win32structures.RECT
+        texts = component.texts()[1:]  ####store this, required, list[str]
+        title = component.window_text()  ####store this, required, str
+        numControls = component.control_count()  ####store this, required, int
+        image = component.capture_as_image()  ####store this, optional, PIL.Image
+        typeOf = component.friendly_class_name()  ####store this, required, str
+    
+        # construct control identifiers
+        # There are 4 possible control identifiers:
+        #   - title
+        #   - friendly class
+        #   - title + friendly class
+        #   - closest text + friendly class (only if the title is empty)
+        controlIdentifiers = [title, typeOf, title + typeOf]  # TODO: Get last control identifier
+    
+        # additional information we can get about uia elements
+        if isinstance(component, pywinauto.controls.uiawrapper):
+            autoID = component.automation_id()  ####store this, optional, int
+            childrenTexts = component.children_texts()  ####store this, optional, list[str]
+            expandState = component.get_expand_state()  ####store this, optional, int
+            shownState = component.get_show_state()  ####store this, optional, int
+    
+        # create a new token
+        # TODO: Reorder parameters to fit Token constructor
+        token = Token(controlIdentifiers, id, isDialog, isEnabled, isVisible, processID, rectangle,
+                      texts, title, numControls, typeOf, autoID, childrenTexts,
+                      expandState, shownState, image, parentTitle, parentType,
+                      topLevelParentTitle, topLevelParentType)
+        
+        return token
+
+    def matchToSuperToken(self, token, parentSuperToken):
+        # determine if the new token matches any super tokens and how well it matches if it does.
+        bestMatch = 0
+        bestDecision = Token.Match.NO
+        selectedSuperToken = None
+        for superToken in self._superTokens:
+            decision, matchVal = superToken.shouldContain(token)
+            bestDecision = min(bestDecision, decision)
+        
+            if decision == Token.Match.NO:
+                continue
+        
+            elif decision == Token.Match.EXACT:
+                return superToken
+        
+            elif decision == Token.Match.CLOSE:
+                # in the case that multiple SuperTokens closely match the token,
+                # we'll use the SuperToken that has the higher match.
+                if matchVal > bestMatch:
+                    bestMatch = matchVal
+                    selectedSuperToken = superToken
+    
+        # No match was found
+        if selectedSuperToken == None and bestMatch != 1:
+            newSuperToken = SuperToken(token)
+            self._superTokens.append(newSuperToken)
+            self.newSuperToken.emit(newSuperToken, parentSuperToken)
+            return newSuperToken
+    
+        # a close match was found
+        else:
+            selectedSuperToken.addToken(token)
+    
+        return selectedSuperToken
+    
+    def play(self):
+        """
+        Runs the Observer.
+        
+        :return: True if the observer is running, False otherwise.
+        :rtype: bool
+        """
+        if not self._process.is_running():
+            return False
+        
+        if self.isRunning():
+            return True
+        
+        self.start()
+        return self.isRunning()
+    
+    def pause(self):
+        """
+        Stops the Observer.
+
+        :return: True if the observer is running, False otherwise.
+        :rtype: bool
+        """
+        if self.isRunning():
+            self.quit()
+            return True
+        return False
