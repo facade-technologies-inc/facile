6640b119fb2b503742df3ea103cd996f3556d8b9
jiuruchen <jiuruchen@email.arizona.edu>
2019-11-26 02:06:08 -0700

    token tag finished

--- src/graphics/tguim/componentgraphics.py
+++ src/graphics/tguim/componentgraphics.py
@@ -335,7 +335,6 @@ class ComponentGraphics(QGraphicsItem):
 			pen.setColor(QColor(0, 0, 0))
 		painter.setPen(pen)
 		
-		# set background color:
 		painter.setBrush(QColor(100, 200, 255))
 		
 		id = self._dataComponent.getId()
@@ -350,6 +349,7 @@ class ComponentGraphics(QGraphicsItem):
 		rectBox = QRectF(self.boundingRect().width() - ComponentGraphics.MARGIN,
 		                 -ComponentGraphics.MARGIN,
 		                 ComponentGraphics.MARGIN * 2, ComponentGraphics.MARGIN * 2)
+		
 		painter.setBrush(QColor(255, 0, 0, 127))
 		painter.drawRect(rectBox)
 		painter.setBrush(QColor(100, 200, 255))
