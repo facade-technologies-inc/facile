dfa10eaa94dd79c299f968794e5c6e1bf1e9db55
Sam Badger <smalbadger@email.arizona.edu>
2020-01-24 21:12:37 -0700

    Feature/splash (#82)
    * Added resources folder and replaces old resources to 'dev_resources'. Created a new branch feature/splashscreen and started on the splash screen py file
    
    * added imports in facile
    
    * got splash screen to come up, however it takes up the whole screen +, still figuring out why that is
    
    * resources folder including facade logo png in it
    
    * used relative path to logo
    
    * trying to add a scale to the logo
    
    * commit to try to adjust the logo scale
    
    * very simple fix.
    
    * finished splash screen
    
    * removed temp dir

---
+++ src/gui/splashscreen.py
@@ -0,0 +1,40 @@
+"""
+
+ /------------------------------------------------------------------------------\
+|                 -- FACADE TECHNOLOGIES INC.  CONFIDENTIAL --                 |
+|------------------------------------------------------------------------------|
+|                                                                              |
+|    Copyright [2019] Facade Technologies Inc.                                 |
+|    All Rights Reserved.                                                      |
+|                                                                              |
+| NOTICE:  All information contained herein is, and remains the property of    |
+| Facade Technologies Inc. and its suppliers if any.  The intellectual and     |
+| and technical concepts contained herein are proprietary to Facade            |
+| Technologies Inc. and its suppliers and may be covered by U.S. and Foreign   |
+| Patents, patents in process, and are protected by trade secret or copyright  |
+| law.  Dissemination of this information or reproduction of this material is  |
+| strictly forbidden unless prior written permission is obtained from Facade   |
+| Technologies Inc.                                                            |
+|                                                                              |
+\------------------------------------------------------------------------------/
+
+This file contains the FacileSplashScreen class which creates a loading window before the application
+is started up.
+
+"""
+from PySide2.QtWidgets import QSplashScreen, QApplication
+from PySide2.QtGui import QPixmap, Qt
+
+class FacileSplashScreen(QSplashScreen):
+	"""
+	FacileSplashScreen class sets the logo picture to display on the splash screen
+	"""
+	def __init__(self):
+		QSplashScreen.__init__(self)
+		self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+		height = QApplication.instance().primaryScreen().size().height() * .25
+		width = QApplication.instance().primaryScreen().size().width() * .25
+		
+		logo = QPixmap('../resources/facile_logo.png').scaledToHeight(height)
+		
+		self.setPixmap(logo)
