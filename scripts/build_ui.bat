8aacbff26d32cec44ddbfa3a46cbc9ab6825e117
smalbadger <smalbadger@email.arizona.edu>
2020-01-29 21:49:18 -0700

    Added script to easily build the ui and resource files.

---
+++ scripts/build_ui.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Compiling Windows and Widgets...
+pyside2-uic ../src/gui/ui/facileview.ui > ../src/gui/ui/ui_facileview.py
+pyside2-uic ../src/gui/ui/copyprojectdialog.ui > ../src/gui/ui/ui_copyprojectdialog.py
+pyside2-uic ../src/gui/ui/newprojectdialog.ui > ../src/gui/ui/ui_newprojectdialog.py
+pyside2-uic ../src/gui/ui/manageprojectdialog.ui > ../src/gui/ui/ui_manageprojectdialog.py
+
+echo Compiling Resource Files...
+pyside2-rcc ../icons.qrc > ../src/gui/rc/icons_rc.py
+
+echo Done.
+
