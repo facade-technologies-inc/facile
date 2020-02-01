23c77db981ad6d22fd3f07a472fd73d1b72d31f4
smalbadger <smalbadger@email.arizona.edu>
2020-02-01 13:45:04 -0700

    Created view for validator

--- scripts/build_ui.bat
+++ scripts/build_ui.bat
@@ -5,6 +5,7 @@ pyside2-uic ../src/gui/ui/facileview.ui > ../src/gui/ui/ui_facileview.py
 pyside2-uic ../src/gui/ui/copyprojectdialog.ui > ../src/gui/ui/ui_copyprojectdialog.py
 pyside2-uic ../src/gui/ui/newprojectdialog.ui > ../src/gui/ui/ui_newprojectdialog.py
 pyside2-uic ../src/gui/ui/manageprojectdialog.ui > ../src/gui/ui/ui_manageprojectdialog.py
+pyside2-uic ../src/gui/ui/validatorview.ui > ../src/gui/ui/ui_validatorview.py
 
 echo Done.
 
---
+++ src/data/validatormessage.py
@@ -0,0 +1,33 @@
+"""
+..
+	/------------------------------------------------------------------------------\
+	|                 -- FACADE TECHNOLOGIES INC.  CONFIDENTIAL --                 |
+	|------------------------------------------------------------------------------|
+	|                                                                              |
+	|    Copyright [2019] Facade Technologies Inc.                                 |
+	|    All Rights Reserved.                                                      |
+	|                                                                              |
+	| NOTICE:  All information contained herein is, and remains the property of    |
+	| Facade Technologies Inc. and its suppliers if any.  The intellectual and     |
+	| and technical concepts contained herein are proprietary to Facade            |
+	| Technologies Inc. and its suppliers and may be covered by U.S. and Foreign   |
+	| Patents, patents in process, and are protected by trade secret or copyright  |
+	| law.  Dissemination of this information or reproduction of this material is  |
+	| strictly forbidden unless prior written permission is obtained from Facade   |
+	| Technologies Inc.                                                            |
+	|                                                                              |
+	\------------------------------------------------------------------------------/
+"""
+from enum import Enum
+
+class ValidatorMessage:
+	class Level(Enum):
+		Error = 3
+		Warning = 2
+		Info = 1
+	
+	def __init__(self, text: str, level: Enum, entity: int=None):
+		self.text = text
+		self.level = level
+		self.entity = entity
+		self.widget = None
