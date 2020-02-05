ff0c7edf05be6f2b1e92c4a9060b494dccbab3fb
jiuruchen <jiuruchen@email.arizona.edu>
2020-02-04 18:05:35 -0700

    starting the validator with note

---
+++ src/data/validator.py
@@ -0,0 +1,7 @@
+# inherited from QThread
+# Look at observer, blinker, explorer. Mainly blinker because it's straight-forward
+
+# run is the most important function.
+# one function for one checking algorithm and call all of them in run
+
+# targetguimodel and aplmodel are the two things
