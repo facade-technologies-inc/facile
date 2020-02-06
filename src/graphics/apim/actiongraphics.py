1c5eb7f84c86c34224ab82c86b897d637a9694fe
andrewkirima <51974854+andrewkirima@users.noreply.github.com>
2020-02-05 19:48:21 -0700

    Drew a rectangle graphic.

--- src/graphics/apim/actiongraphics.py
+++ src/graphics/apim/actiongraphics.py
@@ -22,8 +22,11 @@ This module contains the ActionGraphics() Class.
 """
 import sys
 
-from PySide2.QtWidgets import QGraphicsItem
+from PySide2.QtWidgets import QGraphicsItem, QApplication, QGraphicsView, QGraphicsScene, \
+	QWidget, QStyleOptionGraphicsItem
+from PySide2.QtGui import QPainter, QPainterPath
 from PySide2.QtCore import QRectF
+
 from data.apim.action import Action
 
 class ActionGraphics(QGraphicsItem):
@@ -32,7 +35,8 @@ class ActionGraphics(QGraphicsItem):
 	"""
 	
 	PEN_WIDTH = 1.0
-
+	#
+	SPACE = 10
 	def __init__(self, action: 'Action', parent=None) -> 'ActionGraphics':
 		"""
 		Constructs a Action Graphics object for the given action.
@@ -57,9 +61,43 @@ class ActionGraphics(QGraphicsItem):
 		:rtype: QRectF
 		"""
 		halfPenWidth = ActionGraphics.PEN_WIDTH/2
+		#add PortGraphics.WIDTH & HEIGHT
+		width = 100 + halfPenWidth
+		height = 100 + halfPenWidth
 		
-		return QRectF(action.getInputPorts+halfPenWidth )
+		return QRectF(width, height, width, height)
+	
+	def shape(self) -> QPainterPath:
+		"""
 		
-		
+		:return:
+		"""
+		return QGraphicsItem.shape(self)
+	
+	def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, index: QWidget) -> None:
+		"""
+		Paint a checkbox without the label.
+
+		:param painter: This draws the widget.
+		:type painter: QStylePainter
+		:param option: Option for the style of checkbox.
+		:type option: QStyleOptionViewItem
+		:param index: Index for the painted checkbox.
+		:type index: QModelIndex
+		:return: None
+		:rtype: NoneType
+		"""
+		#painter.drawRect(self.boundingRect())
+		painter.drawPath(self.shape())
 		
 	
+if __name__ == "__main__":
+	
+	app = QApplication()
+	v = QGraphicsView()
+	s = QGraphicsScene()
+	v.setScene(s)
+	A = ActionGraphics(Action)
+	s.addItem(A)
+	v.show()
+	sys.exit(app.exec_())
