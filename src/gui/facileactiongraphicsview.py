81392fe452872a9ef8940da9bb9dfc5a51533d18
Sam Badger <smalbadger@email.arizona.edu>
2020-02-14 13:04:21 -0700

    Can now add action pipelines to project. (#127)
    * Created structure for port editor
    
    * beefed up apim a bit and added a button to add action pipelines.
    
    * The "Add Action Pipeline" button sends a signal with the new action pipeline.
    
    * connected "add action pipeline" to the port editor.
    
    * Can show the action pipelines in the menu and in the graphics view, but only when they are created initially.
    
    * removed print statements
    
    * should not have committed autosaved file

---
+++ src/gui/facileactiongraphicsview.py
@@ -0,0 +1,43 @@
+"""
+..
+	/------------------------------------------------------------------------------\
+	|                 -- FACADE TECHNOLOGIES INC.  CONFIDENTIAL --                 |
+	|------------------------------------------------------------------------------|
+	|                                                                              |
+	|    Copyright [2019] Facade Technologies Inc.                                 |
+	|    All Rights Reserved.                                                      |
+	|                                                                              |
+	| NOTICE:  All information contained herein is, and remains the property of    |
+	| Facade Technologies Inc. and its suppliers if any.  The intellectual and     |
+	| and technical concepts contained herein are proprietary to Facade            |
+	| Technologies Inc. and its suppliers and may be covered by U.S. and Foreign   |
+	| Patents, patents in process, and are protected by trade secret or copyright  |
+	| law.  Dissemination of this information or reproduction of this material is  |
+	| strictly forbidden unless prior written permission is obtained from Facade   |
+	| Technologies Inc.                                                            |
+	|                                                                              |
+	\------------------------------------------------------------------------------/
+	
+This module contains the FacileGraphicsView class which is just like a normal graphics
+view, but can be zoomed.
+"""
+
+
+from PySide2.QtWidgets import QGraphicsScene
+from gui.facilegraphicsview import FacileGraphicsView
+from graphics.apim.actionpipelinegraphics import ActionPipelineGraphics
+
+
+class FacileActionGraphicsView(FacileGraphicsView):
+	"""
+	This class adds functionality to the QGraphicsView to zoom in and out.
+	
+	This is primarily used as the view that shows the target GUI model and API model
+	"""
+	
+	def showAction(self, action: 'Action') -> None:
+		newScene = QGraphicsScene()
+		newScene.addItem(ActionPipelineGraphics(action))
+		self.setScene(newScene)
+	
+
---
+++ src/gui/porteditorwidget.py
@@ -0,0 +1,81 @@
+"""
+..
+	/------------------------------------------------------------------------------\
+	|                 -- FACADE TECHNOLOGIES INC.  CONFIDENTIAL --                 |
+	|------------------------------------------------------------------------------|
+	|                                                                              |
+	|    Copyright [2019] Facade Technologies Inc.                                 |
+	|    All Rights Reserved.                                                      |
+	|                                                                              |
+	| NOTICE:  All information contained herein is, and remains the property of    |
+	| Facade Technologies Inc. and its suppliers if any.  The intellectual and     |
+	| and technical concepts contained herein are proprietary to Facade            |
+	| Technologies Inc. and its suppliers and may be covered by U.S. and Foreign   |
+	| Patents, patents in process, and are protected by trade secret or copyright  |
+	| law.  Dissemination of this information or reproduction of this material is  |
+	| strictly forbidden unless prior written permission is obtained from Facade   |
+	| Technologies Inc.                                                            |
+	|                                                                              |
+	\------------------------------------------------------------------------------/
+This module contains the PortEditorWidget which allows the user to configure a single
+port.
+"""
+
+from PySide2.QtWidgets import QWidget
+from gui.ui.ui_porteditorwidget import Ui_Form as Ui_PortEditorWidget
+from data.apim.port import Port
+
+class PortEditorWidget(QWidget):
+	"""
+	The PortEditorWidget is used to edit a single port object.
+	"""
+	
+	def __init__(self, port: 'Port' = None) -> 'PortEditorWidget':
+		"""
+		Creates a port editor widget.
+		
+		:param port: The port to edit.
+		:type port: Port
+		"""
+		QWidget.__init__(self)
+		self.ui = Ui_PortEditorWidget()
+		self.ui.setupUi(self)
+		
+		if port is not None:
+			self._port = port
+		else:
+			self._port = Port()
+		self.updateEditor()
+		
+			
+	def getPort(self) -> 'Port':
+		"""
+		Get's the port object associated with this port editor.
+		
+		:return: The Port being edited.
+		:rtype:
+		"""
+		return self._port
+	
+	def updateEditor(self) -> None:
+		"""
+		Updates the editor widget to reflect the port values
+		
+		:return: None
+		:rtype: NoneType
+		"""
+		self.ui.nameEdit.setText(self._port.getName())
+		self.ui.typeEdit.setText(str(self._port.getDataType()))
+		self.ui.checkBoxOptional.setChecked(self._port.isOptional())
+		
+	
+	def updatePort(self) -> None:
+		"""
+		Sets all the values of the port values to the values of the editors
+		
+		:return: None
+		:rtype: NoneType
+		"""
+		self._port.setName(self.ui.nameEdit.text())
+		self._port.setDataType(int) #TODO: use type from editor
+		self._port.setOptional(self.ui.checkBoxOptional.isChecked())
