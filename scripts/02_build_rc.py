1f93d6608ecf035db647a582191b002f9d3b40ea
Sam Badger <smalbadger@email.arizona.edu>
2020-03-01 01:55:43 -0700

    edit icons_rc.py in script to get rid of alternating blank lines

--- scripts/02_build_rc.py
+++ scripts/02_build_rc.py
@@ -4,7 +4,7 @@ import subprocess
 qrc = os.path.abspath("../icons.qrc")
 out = os.path.abspath("../src/gui/rc/icons_rc.py")
 
-print("Compiling Resource Files... ", end="")
+print("Compiling Resource Files... ")
 if os.path.exists(os.path.abspath("../venv/")):
     python = os.path.abspath("../venv/Scripts/python.exe")
     rcc = os.path.abspath("../venv/Scripts/pyside2-rcc.exe")
@@ -17,5 +17,16 @@ else:
     with open(out, 'w') as fout:
         proc = subprocess.Popen(["pyside2-rcc", qrc], stdout=fout)
         return_code = proc.wait()
+
+print("Reworking Resource Files...")
+# read in all lines from the rc file
+with open(out, 'r') as f:
+    lines = f.readlines()
+
+# write out only the non-empty lines back to the rc file
+with open(out, 'w') as f:
+    for line in lines:
+        if line.strip():
+            f.write(line)
 
 print("Done")
