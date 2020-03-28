0c3b439b22dfb1a2ed7ac7e0d43c77bb67132c1a
jiuruchen <jiuruchen@email.arizona.edu>
2020-03-28 00:12:29 -0700

    draft for sphinx file generation

--- src/data/compiler/documentationgenerator.py
+++ src/data/compiler/documentationgenerator.py
@@ -1,3 +1,20 @@
-import subprocess
+import os
+import data.statemachine as sm
 
-subprocess.call('ls')
+#projectDir = sm.StateMachine.instance._project.getProjectDir()
+#print(projectDir)
+projectDir = r"C:\Users\ramos\Desktop\FacadeTechnology\FacileAPIs\NotePadAPIDemoExample"
+
+formatChoice = "html"
+
+sphinxFacileDir = r"C:\Users\ramos\Desktop\FacadeTechnology\facile\src\data\compiler\sphinx_src"
+
+os.chdir(projectDir)
+os.system('ls '
+          '& xcopy {0} /e'
+          '& cd src'
+          '& make {1}'
+          '& cd _build'
+          '& move html {2}\Documentation'
+          '& cd ../..'
+          '& RMDIR /Q/S src' .format(sphinxFacileDir, formatChoice, projectDir))
