d7bcd50f90b4970004f1704dff71c4d13fb6df87
Sam Badger <smalbadger@email.arizona.edu>
2020-04-05 17:27:46 -0700

    got executable and installer working.

---
+++ setup.py
@@ -0,0 +1,48 @@
+import sys
+import os
+from cx_Freeze import setup, Executable
+
+sys.path.append(os.path.abspath("./src/"))
+sys.path.append(os.path.abspath("./src/gui/rc/"))
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = {
+    "packages": ["src.gui",
+                 "src.qt_models",
+                 "src.data",
+                 "src.libs",
+                 "src.tguiil",
+                 "src.graphics"
+                 ],
+    "includes": ["scipy.sparse.csgraph._validation",
+                 "scipy.ndimage._ni_support",
+                 "scipy._distributor_init"
+                 ],
+    "include_files": ["database/"],
+    "excludes": []
+}
+
+installOptions = {}
+
+bdistOptions = {}
+
+base = None
+
+# Uncomment for GUI applications to NOT show cmd window while running.
+if sys.platform=='win32':
+    base = 'Win32GUI'
+
+executables = [
+    Executable('src/facile.py', base=base, targetName = 'facile.exe')
+]
+
+setup(name='Facile',
+      version = '1.0',
+      description = 'A platform for generating Python APIs used to control graphical user interfaces.',
+      options = {
+          "build_exe": buildOptions,
+          "install_exe": installOptions,
+          "bdist_msi": bdistOptions,
+      },
+      executables = executables)
