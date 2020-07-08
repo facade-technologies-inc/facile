35d838d3b9c0ac52cf5b9583c68886e263a99a31
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2020-07-08 00:09:09 -0700

    removed __file__ variable from code that will be executed by the Facile process.
    I did not removed instances of the __file__ variable located in src/gui/frame/_utils.py because that code will move elsewhere soon.

--- src/tools/doc_generator/sphinx_src/src/conf.py
+++ src/tools/doc_generator/sphinx_src/src/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Context used for imports ------------------------------------------------
 
 import libs.env as env
-env.CONTEXT = "Sphinx"
+env.updateContext("Sphinx")
 
 # -- Project information -----------------------------------------------------
 
